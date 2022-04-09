9854bcc92a7e4cffb05d11524ee9824999c30528
lokithe5th <lourenslinde@gmail.com>
2022-04-09 06:59:48 +0200

    Added ERC20 functions

--- packages/hardhat/contracts/Worlds.sol
+++ packages/hardhat/contracts/Worlds.sol
@@ -2,6 +2,7 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -22,6 +23,8 @@ contract Worlds is ERC721, Ownable {
   Counters.Counter private _tokenIds;
 
   uint256 private mintDeadline;
+  IERC20 private energyToken;
+  uint256 private tokenPrice;
 
   constructor() ERC721("Worlds", "WORLDS") {
       // Incrementing _tokenIds in the constructor lowers gas costs for the first mint
@@ -33,6 +36,8 @@ contract Worlds is ERC721, Ownable {
 
   /// Mapping of tokenId to rand used to generate properties
   mapping (uint256 => bytes32) public properties;
+
+  mapping (uint256 => uint256) private lastExtraction;
 
   /// The array of different climates a planet might have
   string[] private climates = [
@@ -114,6 +119,23 @@ contract Worlds is ERC721, Ownable {
     "of the Sublimed",
     "of empowerment"
   ];
+
+  function claimTokens(address payable to, uint256 tokenId) external returns (uint256) {
+    require(msg.sender == ownerOf(tokenId),"This world's energy is not yours to claim.");
+    uint256 tokensClaimable = worldEnergy(tokenId);
+    require(energyToken.transfer(to, tokensClaimable), "Energy transfer failed!");
+    lastExtraction[tokenId] = block.timestamp;
+    return tokensClaimable;
+  }
+
+  function exchangeTokensForEth(address payable to, uint256 amount) external returns (uint256) {
+    uint256 totalEth = tokenPrice*amount;
+    require(energyToken.approve(address(this), amount), "Must approve tokens for spending.");
+    require(energyToken.transferFrom(msg.sender, address(this), amount));
+    (bool sent, ) = to.call{value: totalEth}("");
+    require(sent, "Failed to send Ether");
+    return totalEth;
+  }
 
   function predictableRandom() internal view returns(bytes32) {
     return keccak256(abi.encodePacked( blockhash(block.number-1), msg.sender, address(this) ));
@@ -223,6 +245,7 @@ contract Worlds is ERC721, Ownable {
 
     properties[id] = rand;
     _mint(msg.sender, id);
+    lastExtraction[id] = block.timestamp;
     _tokenIds.increment();
 
     return id;
@@ -285,7 +308,7 @@ contract Worlds is ERC721, Ownable {
       //string memory description = string(abi.encodePacked('This Sphere is the color #',color[id].toColor(),' with a energy capacity of ',uint2str(energy[id]),'!!!'));
       //string memory image = Base64.encode(bytes(generateSVGofTokenById(id)));
 
-      string[16] memory parts;
+      string[18] memory parts;
         parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: monospace; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" /><text x="10" y="20" class="base">';
 
         parts[1] = getType(id);
@@ -316,10 +339,14 @@ contract Worlds is ERC721, Ownable {
 
         parts[14] = '</text><text x="10" y="160" class="base">';
 
-        parts[15] = '</text></svg>';
+        parts[15] = uint2str(worldEnergy(id));
+
+        parts[16] = '</text><text x="10" y="160" class="base">';
+
+        parts[17] = '</text></svg>';
 
         string memory svg = string(abi.encodePacked(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7], parts[8]));
-        svg = string(abi.encodePacked(svg, parts[9], parts[10], parts[11], parts[12], parts[13], parts[14], parts[15]));
+        svg = string(abi.encodePacked(svg, parts[9], parts[10], parts[11], parts[12], parts[13], parts[14], parts[15], parts[16], parts[17]));
 
     return svg;
   }
@@ -332,6 +359,10 @@ contract Worlds is ERC721, Ownable {
 
   function totalSupply() public view returns (uint256) {
     return _tokenIds.current();
+  }
+
+  function worldEnergy(uint256 id) public view returns (uint256) {
+    return (lastExtraction[id] / 1 days)*(getEnergyLevel(id)/100);
   }
 
   function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
